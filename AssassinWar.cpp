6b9a6afe09d9978b33701d1908f3758c697f7d7f
TyrealGray <tyrealgray@ymail.com>
2014-02-07 23:14:32 +0800

    Signed-off-by: TyrealGray <tyrealgray@ymail.com>

--- AssassinWar.cpp
+++ AssassinWar.cpp
@@ -12,9 +12,9 @@ AssassinWar::AssassinWar(QWidget *parent, Qt::WFlags flags)
 {
     ui.setupUi(this);
 
-    showFullScreen();
-
-    ui.mainToolBar->hide();
+    MapLoader mapLoader;
+    QWidget* pMapWidget =  mapLoader.LoadMap("./map/map1.ui");
+    mapLoader.LoadMapTerrain(*pMapWidget);
 
     m_pRepaintTimer = new QTimer(this);
     connect(m_pRepaintTimer, SIGNAL(timeout()), this, SLOT(repaint()));
