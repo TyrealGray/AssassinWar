5d962c1453d069fae1daaeb0e7f0b70bba037d2d
TyrealGray <tyrealgray@ymail.com>
2014-02-07 20:27:57 +0800

    change name
    change some name

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	AssassinWar w;
-	w.show();
-	return a.exec();
+    QApplication AW_App(argc, argv);
+    AssassinWar AW;
+    AW.show();
+    return AW_App.exec();
 }
