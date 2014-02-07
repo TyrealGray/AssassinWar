e17f6ab906162ec616a632807eff131ed2ba8279
TyrealGray <tyrealgray@ymail.com>
2014-02-07 23:03:31 +0800

    commit test
    commit test

--- AssassinWar.cpp
+++ AssassinWar.cpp
@@ -12,9 +12,9 @@ AssassinWar::AssassinWar(QWidget *parent, Qt::WFlags flags)
 {
     ui.setupUi(this);
 
-    //MapLoader mapLoader;
-    //QWidget* pMapWidget =  mapLoader.LoadMap("./map/map1.ui");
-    //mapLoader.LoadMapTerrain(*pMapWidget);
+    MapLoader mapLoader;
+    QWidget* pMapWidget =  mapLoader.LoadMap("./map/map1.ui");
+    mapLoader.LoadMapTerrain(*pMapWidget);
 
     m_pRepaintTimer = new QTimer(this);
     connect(m_pRepaintTimer, SIGNAL(timeout()), this, SLOT(repaint()));
@@ -28,13 +28,13 @@ AssassinWar::~AssassinWar()
 
 void AssassinWar::paintEvent(QPaintEvent *PaintEvent)
 {
-    static float a = 0.0f;
+    //static float a = 0.0f;
 
-    if(60 < a)
-    {
-        a = 0.0f;
-    }
+    //if(60 < a)
+    //{
+    //    a = 0.0f;
+    //}
 
-    QPainter painter(this);
-    painter.drawLine(++a, 0.0, 15.0, 25.0);// drawing code
+    //QPainter painter(this);
+    //painter.drawLine(++a, 0.0, 15.0, 25.0);// drawing code
 }
